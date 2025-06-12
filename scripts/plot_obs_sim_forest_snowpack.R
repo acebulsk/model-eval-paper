74578773f0b947ea2c1bb67d0907b141f0da5183
Alex Cebulski <alexcebulski@gmail.com>
2025-06-11 17:53:17 -0700

    plot all stations on one and do stats

--- scripts/plot_obs_sim_forest_snowpack.R
+++ scripts/plot_obs_sim_forest_snowpack.R
@@ -8,10 +8,11 @@ path <- "crhm/output/"
 
 ### obs snow ----
 
-snow_survey <- CRHMr::readObsFile(
+fortress_snow_survey <- CRHMr::readObsFile(
   'data/fortress/snow_survey/Powerline_obs_SWE_2013-23.obs',
   timezone = 'Etc/GMT+6'
-) |> select(datetime, obs_swe = Powerline_Forest.1)
+) |> select(datetime, obs_swe = Powerline_Forest.1) |> 
+  mutate(station = 'Fortress - Powerline Forest')
 
 # Read the file while skipping non-relevant lines
 snow_scale <- read.table('data/fortress/snow_survey/Powerline_SommerSnowScale_SWE_29Oct2013-16Oct2023.obs', 
@@ -28,10 +29,10 @@ snow_scale <- CRHMr::makeRegular(snow_scale, 'Etc/GMT+6')
 ### updated crhm (cansnobal) ----
 
 prj <- "fortress_powerline_clearing_snowsurveytransect_cansnobal"
-run_tag_updt <- "v_4_0_r3"
+run_tag_updt <- "v_4_0_output"
 
 crhm_output_new <- read_crhm_obs(path, prj, run_tag_updt, 'Etc/GMT+6') |> 
-  select(datetime, Simulated_forest_new = SWE.1)
+  select(datetime, CP25 = SWE.1)
 
 ### baseline crhm ----
 prj <- "fortress_powerline_clearing_snowsurveytransect_baseline"
@@ -43,12 +44,15 @@ crhm_output_base <- read_crhm_obs(
   runtag = run_tag_base,
   tz = 'Etc/GMT+6'
 ) |>
-  select(datetime, Simulated_forest_base = SWE.1)
+  select(datetime, E10 = SWE.1)
 
 # ggplot(crhm_output_base, aes(datetime, SWE.1)) + 
 #   geom_line()
 
 ### combine powerline ----
+
+fortress_obs_sim_swe <-   left_join(crhm_output_new, crhm_output_base, by = 'datetime') |> 
+  mutate(station = 'Fortress - Powerline Forest')
 
 snowscale_and_new <- left_join(crhm_output_new,
                                snow_scale,
@@ -61,18 +65,19 @@ snowscale_and_new <- left_join(crhm_output_new,
 
 ### obs snow ----
 
-snow_survey <- CRHMr::readObsFile(
+marmot_snow_survey <- CRHMr::readObsFile(
   'data/marmot/Marmot_UpperForest_UpperClearing_SWE_2008-23.obs',
   timezone = 'Etc/GMT+6'
-) |> select(datetime, obs_swe = Upper_Forest.1)
+) |> select(datetime, obs_swe = Upper_Forest.1) |>
+  mutate(station = 'Marmot - Upper Forest')
 
 ### updated crhm (cansnobal) ----
 
 prj <- "marmot_upper_forest_clearing_snowsurveytransect_cansnobal"
-run_tag_updt <- "wind_18_m"
+run_tag_updt <- "alpha_0.65"
 
 crhm_output_new <- read_crhm_obs(path, prj, run_tag_updt, 'Etc/GMT+6') |> 
-  select(datetime, Simulated_forest_new = SWE.1)
+  select(datetime, CP25 = SWE.1)
 
 ### baseline crhm ----
 prj <- "marmot_upper_forest_clearing_snowsurveytransect_baseline"
@@ -84,100 +89,101 @@ crhm_output_base <- read_crhm_obs(
   runtag = run_tag_base,
   tz = 'Etc/GMT+6'
 ) |>
-  select(datetime, Simulated_forest_base = SWE.1)
+  select(datetime, E10 = SWE.1)
 
 # ggplot(crhm_output_base, aes(datetime, SWE.1)) + 
 #   geom_line()
 
 ### combine marmot ----
 
-snowscale_and_new <- left_join(crhm_output_new,
+marmot_obs_sim_swe <- left_join(crhm_output_new,
                                crhm_output_base,
                                by = 'datetime') |>
-  mutate(station = 'Marmot Upper Forest')
+  mutate(station = 'Marmot - Upper Forest')
 
 
 ## WOLF CREEK ----
 
 ### Snow survey obs ---- 
 
-snow_survey <- CRHMr::readObsFile(
-  'data/wolf-creek/snow_survey/WolfCreek_Forest_observed_SWE_1999.obs',
-  timezone = 'Etc/GMT+7'
-) |> select(datetime, obs_swe = Forest_SWE.1)
-
-### updated crhm (cansnobal) ----
-
-prj <- "wolf_creek_forest_snowsurveytransect_cansnobal"
-run_tag_updt <- "v_4_0_r5"
-
-crhm_output_new <- read_crhm_obs(path, prj, run_tag_updt, 'Etc/GMT+6') |> 
-  select(datetime, Simulated_forest_new = SWE.1, Simulated_open_new = SWE.2)
-
-### baseline crhm ----
-
-prj <- "wolf_creek_borland_example"
-run_tag_updt <- "r1"
-
-crhm_output_base <- read_crhm_obs(path, prj, run_tag_updt, 'Etc/GMT+6') |> 
-  select(datetime, Simulated_forest_base = SWE.3)
-
-### combine wolf ----
-
-snowscale_and_new <- left_join(crhm_output_new,
-                               crhm_output_base,
-                               by = 'datetime') |>
-  mutate(station = 'Wolf Creek Forest')
-
-ggplot(snowscale_and_new |> pivot_longer(starts_with('Simulated_')), aes(datetime, value, colour = name)) +
-  geom_line()
-
+# snow_survey <- CRHMr::readObsFile(
+#   'data/wolf-creek/snow_survey/WolfCreek_Forest_observed_SWE_1999.obs',
+#   timezone = 'Etc/GMT+7'
+# ) |> select(datetime, obs_swe = Forest_SWE.1)
+# 
+# ### updated crhm (cansnobal) ----
+# 
+# prj <- "wolf_creek_forest_snowsurveytransect_cansnobal"
+# run_tag_updt <- "v_4_0_r5"
+# 
+# crhm_output_new <- read_crhm_obs(path, prj, run_tag_updt, 'Etc/GMT+6') |> 
+#   select(datetime, CP25 = SWE.1, Simulated_open_new = SWE.2)
+# 
+# ### baseline crhm ----
+# 
+# prj <- "wolf_creek_borland_example"
+# run_tag_updt <- "r1"
+# 
+# crhm_output_base <- read_crhm_obs(path, prj, run_tag_updt, 'Etc/GMT+6') |> 
+#   select(datetime, E10 = SWE.3)
+# 
+# ### combine wolf ----
+# 
+# snowscale_and_new <- left_join(crhm_output_new,
+#                                crhm_output_base,
+#                                by = 'datetime') |>
+#   mutate(station = 'Wolf Creek Forest')
+# 
+# ggplot(snowscale_and_new |> pivot_longer(starts_with('Simulated_')), aes(datetime, value, colour = name)) +
+#   geom_line()
+# 
 
 ## RUSSELL CREEK ----
 
 ### Snow survey obs ---- 
 
-snow_survey <- CRHMr::readObsFile(
-  'data/russell-creek/2006-2007 Upper Stephanie-SWE.obs',
-  timezone = 'Etc/GMT+8'
-) |> select(datetime, obs_swe = Forest_SWE.8) |>  # 8 is USOG2
-mutate(obs_swe = ifelse(obs_swe == 0, NA, obs_swe))
+russell_snow_survey <- 
+  readRDS('data/russell-creek/russell_upper_stephanie_all_swe_2006_2008.rds') |> 
+  select(datetime, obs_swe = OG2)  |>
+  mutate(station = 'Russell - US Old Growth')
 
 ### updated crhm (cansnobal) ----
 
 prj <- "russell_upper_steph_forest_snowsurveytransect_cansnobal"
-run_tag_updt <- "r3"
+run_tag_updt <- "new_obs5"
 
 crhm_output_new <- read_crhm_obs(path, prj, run_tag_updt, 'Etc/GMT+8') |> 
-  select(datetime, Simulated_forest_new = SWE.1)
+  select(datetime, CP25 = SWE.1) |>
+  mutate(station = 'Russell - US Old Growth')
 
-ggplot(crhm_output_new, aes(datetime, Simulated_forest_new)) + geom_line()
 
 ### baseline crhm ----
 
-prj <- "wolf_creek_borland_example"
-run_tag_updt <- "r1"
+prj <- "russell_upper_steph_forest_snowsurveytransect_baseline"
+run_tag_updt <- "new_obs"
 
-crhm_output_base <- read_crhm_obs(path, prj, run_tag_updt, 'Etc/GMT+6') |> 
-  select(datetime, Simulated_forest_base = SWE.3)
+crhm_output_base <- read_crhm_obs(path, prj, run_tag_updt, 'Etc/GMT+8') |> 
+  select(datetime, E10 = SWE.1)
 
 ### combine russell ----
 
-snowscale_and_new <- crhm_output_new |>
-  mutate(station = 'Russell Creek US Old Growth')
+russell_obs_sim_swe <- left_join(crhm_output_new, crhm_output_base, by = 'datetime') |>
+  mutate(station = 'Russell - US Old Growth')
 
-ggplot(snowscale_and_new |> pivot_longer(starts_with('Simulated_')), aes(datetime, value, colour = name)) +
-  geom_line()
-
-# PLOT NEW and Snow Scale ---- 
+# PLOT ALL STATIONS ---- 
 
 # SWE ---- 
 
-# left_join(crhm_output_tree |> 
-#             select(datetime, Snow_load.1_15min = Snow_load.1))
+all_sites_mods <- 
+  rbind(marmot_obs_sim_swe, 
+        russell_obs_sim_swe) |> 
+  rbind(fortress_obs_sim_swe)
+
+all_sites_obs <- 
+  rbind(marmot_snow_survey, russell_snow_survey, fortress_snow_survey)
 
 # Plot subcanopy SWE
-snowscale_and_new |> 
+all_sites_mods |> 
   # rename(Observed = SWE,
   #        Simulated = SWE.1) |> 
   pivot_longer(
@@ -186,34 +192,35 @@ snowscale_and_new |>
   ggplot(aes(x = datetime, y = value, colour = name)) +
   geom_line() +
   geom_point(
-    data = snow_survey, 
-    aes(x = datetime, y = obs_swe, colour = "Snow Survey"), 
+    data = all_sites_obs,
+    aes(x = datetime, y = obs_swe, colour = "Snow Survey"),
     inherit.aes = FALSE
-  ) #+
-  # scale_colour_manual(
-  #   values = c("Observed_clearing" = "blue", 
-  #              "Simulated_forest_base" = "red", 
-  #              "Simulated_forest_new" = "green", 
-  #              "Snow Survey" = "black"),
-  #   # labels = c(
-  #   #   "Observed" = "Observed-Clearing",
-  #   #   "Simulated" = "Simulated-Forest"
-  #   # ),
-  #   name = "Legend"
-  # ) +
+  ) +
+  facet_wrap(~station, nrow = 4, scales = 'free') +
+  scale_colour_manual(
+    values = c(#"Observed_clearing" = "blue",
+               "E10" = "salmon",
+               "CP25" = "dodgerblue",
+               "Snow Survey" = "black"),
+    # labels = c(
+    #   "Observed" = "Observed-Clearing",
+    #   "Simulated" = "Simulated-Forest"
+    # ),
+    name = "Legend"
+  ) +
   # guides(colour = guide_legend(override.aes = list(
   #   linetype = c(1, 1, 1, 0), # Line styles for the first two, none for points
   #   shape = c(NA, NA, NA, 16)  # Points only for "Snow Survey"
   # ))) +
-  # ylab(expression(Snow~Water~Equivalent~(kg~m^{-2}))) +
-  # xlab(element_blank()) +
-  # theme(legend.position = 'bottom')
+  ylab(expression(Snow~Water~Equivalent~(kg~m^{-2}))) +
+  xlab(element_blank()) +
+  theme(legend.position = 'bottom')
 
 ggsave(
   paste0(
     'figs/crhm-analysis/crhm_swe_vs_snow_survey/crhm_swe_vs_snow_survey_vs_snowscale_timeseries_',
-    '_',
-    run_tag_updt,
+    # '_',
+    # run_tag_updt,
     '_',
     format(Sys.time(), "%Y-%m-%d_%H-%M-%S"),
     '.png'
@@ -241,24 +248,22 @@ plotly::ggplotly()
 
 ## SWE ----
 
-mod_snow_survey <- left_join(
-  snow_survey,
-  snowscale_and_new |> select(datetime, Baseline = Simulated_forest_base, Updated = Simulated_forest_new)
+obs_mod <- left_join(
+  all_sites_mods, all_sites_obs, suffix = c('sim', 'obs')
 ) |> 
-  pivot_longer(c(Baseline, Updated))
+  pivot_longer(c(E10, CP25))
 
-ggplot(mod_snow_survey,
+ggplot(obs_mod,
        aes(obs_swe, value, colour = name)) +
   geom_point() + geom_abline() +
   ylab(expression(Simulated~Snow~Water~Equivalent~(kg~m^{-2}))) +
   xlab(expression(Observed~Snow~Water~Equivalent~(kg~m^{-2}))) +
-  theme(legend.title = element_blank())
+  theme(legend.title = element_blank()) +
+  facet_wrap(~station, scales = 'free')
 
 ggsave(
   paste0(
     'figs/crhm-analysis/crhm_swe_vs_snow_survey/crhm_swe_vs_snow_survey_scatter_',
-    '_',
-    run_tag_updt,
     '_',
     format(Sys.time(), "%Y-%m-%d_%H-%M-%S"),
     '.png'
@@ -270,9 +275,9 @@ ggsave(
 
 # error table ----
 
-mod_snow_survey_err_tbl <- mod_snow_survey |> 
+mod_snow_survey_err_tbl <- obs_mod |> 
   mutate(diff = obs_swe - value) |> 
-  group_by(name) |> 
+  group_by(name, station) |> 
   summarise(
     `Mean Bias` = mean(diff, na.rm = T),
     MAE = mean(abs(diff), na.rm = T),
@@ -282,12 +287,10 @@ mod_snow_survey_err_tbl <- mod_snow_survey |>
     R = cor(obs_swe, value),
     `r^2` = R^2) |> 
   mutate(across(`Mean Bias`:`r^2`, round, digits = 3))
-
+mod_snow_survey_err_tbl
 write_csv(mod_snow_survey_err_tbl,
           paste0(
             'tbls/crhm-swe-vs-snowsurvey-errortbl',
-            '_',
-            run_tag_updt,
             '_',
             format(Sys.time(), "%Y-%m-%d_%H-%M-%S"),
             '.csv'
