16d3f5ec0756d01e6f79f6b19a38e74fca91a0da
Alex Cebulski <alexcebulski@gmail.com>
2025-09-04 15:18:51 -0600

    add inset map

--- scripts/gis/regional-scale-map.R
+++ scripts/gis/regional-scale-map.R
@@ -2,8 +2,13 @@ library(sf)
 library(tidyverse)
 library(tmap)
 library(ggrepel)
+library(terra)
+library(tidyterra)
+library(spData)
+library(cowplot)
 
-data(land)
+# https://www.cec.org/north-american-environmental-atlas/land-cover-30m-2020/
+globcover <- terra::rast('~/Documents/no-backups/land_cover_2020v2_30m_tif/NA_NALCMS_landcover_2020v2_30m/data/NA_NALCMS_landcover_2020v2_30m.tif')
 
 sites <- data.frame(
   site = c('Wolf Creek', 'Russell Creek', 'Fortress Mountain', 'Marmot Creek'),
@@ -11,50 +16,106 @@ sites <- data.frame(
   lat = c(60.567, 50.3710, 50.8269, 50.9255877)
 )
 
-sites_sf <- sf::st_as_sf(sites, coords = c('lon', 'lat'), crs = st_crs(land))
+sites_sf <- sf::st_as_sf(sites, coords = c('lon', 'lat'), crs = 4326)
+sites_laea <- st_transform(sites_sf, crs(globcover))
 
-# sf::write_sf(sites_sf, 'data/gis/site_coords.gpkg')
+xmin <- -2120000
+xmax <- -900864
+ymin <- 570000
+ymax <- 2547041
 
-bb_canusa <- st_bbox(c(
-  xmin = -165,
-  ymin =  35,
-  xmax = -85,
-  ymax =  75
-), crs = st_crs(4326)) # assuming WGS 84
 
-land_crop <- sf::st_crop(land, bb_canusa) |> 
-  select(elevation)
+# Create terra extent
+sites_extent <- ext(xmin, xmax, ymin, ymax)
 
-land_df <- as.data.frame(land_crop, xy = TRUE, na.rm = FALSE)
-ggplot() +
-  geom_raster(data = land_df, aes(x = x, y = y, fill = elevation)) +
-  scale_fill_gradientn(
-    colors = terrain.colors(100),
-    name = "Elevation (m)",
-    na.value = "transparent"
-  ) +
-  geom_sf(data = sites_sf, color = 'red', size = 1.5) +
+# Crop raster
+land_buf <- crop(globcover, sites_extent)
+lvls <- levels(land_buf)[[1]]
+
+lvls$Class_EN <- c(
+  NA,                         # 0
+  "Needleleaf\n vegetation",               # 1
+  "Needleleaf\n vegetation",               # 2
+  "Non-needleleaf\n vegetation",# 3
+  "Non-needleleaf\n vegetation",# 4
+  "Non-needleleaf\n vegetation",# 5
+  "Non-needleleaf\n vegetation",# 6
+  "Non-needleleaf\n vegetation",# 7
+  "Non-needleleaf\n vegetation",# 8
+  "Non-needleleaf\n vegetation",# 9
+  "Non-needleleaf\n vegetation",#10
+  "Non-needleleaf\n vegetation",#11
+  "Non-needleleaf\n vegetation",#12
+  "Barren",                   #13
+  "Water",                  #14
+  "Non-needleleaf\n vegetation",#15
+  "Barren",                   #16
+  "Urban",                    #17
+  "Water",                    #18
+  "Snow/Ice"                  #19
+)
+
+# Assign back to raster
+levels(land_buf) <- lvls
+cat_colors <- c(
+  "Needleleaf\n vegetation" = "#003d00",
+  "Non-needleleaf\n vegetation" = "#55a630",
+  "Barren" = "#a8abaf",
+  "Urban" = "#dc2126",
+  "Water" = "#4c70a3",
+  "Snow/Ice" = "#ffffff"
+)
+
+main_map <- ggplot() +
+  geom_spatraster(data = land_buf, aes(fill = Class_EN)) +
+  scale_fill_manual(
+    values = cat_colors,
+    na.value = "transparent", drop = TRUE) +
+  geom_sf(data = sites_laea, color = "black", fill = "gold", size = 3, shape = 21) +
   geom_label_repel(
     data = sites_sf,
     aes(geometry = geometry, label = site),
+    segment.color = "black",  # change the connecting line color
     stat = "sf_coordinates",
     box.padding = unit(1, "lines"),
     size = 3
   ) +  
-  coord_sf(expand = FALSE) +
-  scale_x_continuous(breaks = seq(-170, -60, by = 30)) +
-  scale_y_continuous(breaks = seq(20, 80, by = 20)) +
+  # coord_sf(expand = FALSE) +
+  # scale_x_continuous(breaks = seq(-170, -60, by = 30)) +
+  # scale_y_continuous(breaks = seq(20, 80, by = 20)) +
   theme_minimal() +
   theme(
-    legend.position = c(0, 0.01),
-    legend.justification = c("left", "bottom"),
-    panel.border = element_rect(color = "black", fill = NA, linewidth = 0.8)
+    legend.background = element_rect(fill = "#FAEBD7", color = "black", size = 0.5),
+    # legend.key = element_rect(fill = "#FAEBD7", color = NA),
+    panel.background = element_rect(fill = "#a6cee360", color = NA),  # light blue ocean
+    panel.grid.major = element_line(color = "gray80", size = 0.2),
+    # Place legend inside
+    legend.position = c(0.93, 0.93),         # x = 0-1, y = 0-1 (top-right corner)
+    legend.justification = c("right", "top"),
+    # panel.border = element_rect(color = "black", fill = NA, linewidth = 0.8)
     # panel.grid.major = element_line(color = "grey80", linetype = "dotted")
-  ) +
-  labs(x = element_blank(),
-       y = element_blank())
+  ) + labs(y = element_blank(), x = element_blank(), fill = 'Land Cover')
 
-ggsave('figs/maps/sit_map_ele.png', width = 5, height = 5, bg = 'white')
+# Inset map
+inset_map <- ggplot() +
+  geom_sf(data = world, fill = "grey80", color = "white") +
+  geom_sf(data = sites_sf, fill = "gold", color = "black", size = 2, shape = 21) +
+  coord_sf(xlim = c(-168, -62), ylim = c(22, 78)) +  # shrink a bit
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = "white", color = "black", linewidth = 0.8)
+  )
+
+final_map <- ggdraw() +
+  draw_plot(main_map) +
+  draw_plot(inset_map, x = 0.7, y = 0.58, width = 0.16, height = 0.16)
+
+ggsave("figs/final/figure1.png", final_map, width = 7.5, height = 10, bg = "white")
+
+
+# ggsave('figs/maps/sit_map_globcov.png', width = 7.5, height = 10, bg = 'white')
+ggsave('figs/final/figure1.png', width = 7.5, height = 10, bg = 'white')
+
 
 # OLDER ONE WITH ECOZONE (missing below)
 
